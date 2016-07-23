02c96fcc22b42e730fb6ed6f33367df8ac22f01a
Deepak Prakash <deepak@Deepaks-MacBook-Pro.local>
2016-07-23 15:05:41 +1000

    text and image drag and drop functionality added

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ $(document).ready (function() {
 
   // add column on click based on conditions
   $(".addColumn").on("click",function(){
-    
+  
     var selectedNumber =  prompt("Type columns between 1 to 12");
     var numOfColumns = parseInt(selectedNumber);
     while (selectedNumber > 12 || selectedNumber == "undefined" || selectedNumber == ""){
@@ -47,6 +47,41 @@ $(document).ready (function() {
       alert("columns already exist in this row");
     }
   });
+
+  // drag and drop text
+  $(".addObject").draggable({
+      revert: "valid",
+      drag: function(event,ui){
+        if (!$('.column').hasClass('ui-droppable')){
+          alert("select column to drop");
+        }
+      }
+  });
+
+  // highlight or select column 
+  var droppableDiv = $("<div></div>");
+  $("#template").on("click",".column",function(e){
+    $("#template .column").removeClass("active ui-droppable");
+    $(e.target).addClass("active ui-droppable");
+    droppableDiv = $(e.target).context;
+    console.log(droppableDiv);
+    $(droppableDiv).droppable({
+      drop: function(event, ui) {
+        if (ui.draggable.attr("id") == "textObject"){
+          $(this).append('<input type="text" name="textbox" id="textbox" value="" >');
+        } else if (ui.draggable.attr("id") == "imageObject"){
+          var image='<img class="image_area" src="1.jpg" >';
+          $(this).append(image);
+        } else {
+          alert("drag text or image from panel");
+        }
+
+      }
+    });
+    
+  });
+
+  
   
 });
 
