ce4b280ab957b29cd5889c75aa1e540681f1183f
deepak6sp <deepak6sp@gmail.com>
2016-10-15 15:04:36 +1100

    draganddrop2 new changes

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 $(document).ready (function() {
-
-	$(".panel").draggable({helper: "clone",
-		revert: true,
-		start: function( event, ui ) {
-		}
+	$("#side_bar .panel").draggable({
+		helper: "clone",
+		revert: true
 	});
 
 
 	$( "#template" ).droppable({
       	accept: ".row",
 		drop: function(event,ui){
-				$(".wrapper .initialRow").remove();
-				$(".wrapper").append($(ui.draggable).clone());
+				$(".wrapper .initialRow").html("");
+				$($(ui.draggable).clone()).insertBefore(".wrapper .initialRow");
+				$(".initialRow").prev().html("");
+				
 				$(".wrapper .row").droppable({
 					accept: ".columns",
 					drop: function(event,ui){
-						//$(this).html("");
-						$(this).append($(ui.draggable).clone());
-						$(".wrapper .columns").html("add modules");
+						$(this).prepend($(ui.draggable).clone());
+						//$(".wrapper .columns").html("add modules");
 					}
 				});
-				$(".wrapper .row").html("add column");
-			}
+				//$(".wrapper .row").html("add column");
+		}
+
     });
+
 });
-
-
-function make_droppable() {
-    // a lots of line with several function
-}
