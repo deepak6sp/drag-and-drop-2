fd1b8e8dd6de3b5986b5bf0eed8e11195186c662
deepak6sp <deepak6sp@gmail.com>
2016-10-15 15:20:13 +1100

    draganddrop2 new changes

--- script.js
+++ script.js
@@ -8,18 +8,16 @@ $(document).ready (function() {
 	$( "#template" ).droppable({
       	accept: ".row",
 		drop: function(event,ui){
-				$(".wrapper .initialRow").html("");
-				$($(ui.draggable).clone()).insertBefore(".wrapper .initialRow");
-				$(".initialRow").prev().html("");
-				
-				$(".wrapper .row").droppable({
-					accept: ".columns",
-					drop: function(event,ui){
-						$(this).prepend($(ui.draggable).clone());
-						//$(".wrapper .columns").html("add modules");
-					}
-				});
-				//$(".wrapper .row").html("add column");
+			$(".wrapper .initialRow").html("");
+			$($(ui.draggable).clone()).insertBefore(".wrapper .initialRow");
+			$(".initialRow").prev().html("");
+			
+			$(".wrapper .row").droppable({
+				accept: ".columns",
+				drop: function(event,ui){
+					$(this).prepend($(ui.draggable).clone());
+				}
+			});
 		}
 
     });
